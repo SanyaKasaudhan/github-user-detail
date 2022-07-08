950ca81cb8d0236e65df92f6a2325947c4b7e188
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2022-07-08 15:02:50 +0530

    data mapped

---
+++ github-users/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import GithubUser from "./Components/GithubUser";
+function App() {
+  return (
+    <>
+    <h1 className="head">Github Users</h1>
+    <div className="container">
+      <div className="row">
+        <GithubUser />
+
+      </div>
+      asd
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ github-users/src/Components/GithubUser.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+const GithubUser = () => {
+
+    const[error,setError]=useState(null);
+    const[isLoaded, setIsLoaded]=useState(false);
+    const[items,setItems]=useState([]);
+    useEffect(()=>{
+        fetch("https://api.github.com/users")
+        .then(res=>res.json())
+        .then((result)=>{
+            setIsLoaded(true);
+            setItems(result);
+        },
+        (error)=>{
+            setIsLoaded(true);
+            setError(error);
+        })
+    },[])
+    
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+  return (
+    <div>
+
+        <CardGroup style={{width:"15em", height:"25em" , marginTop:"1em"}}>
+        <Card style={{ width: '8rem' }}>
+        {items.map(item => (  
+          <>
+            <Card.Img variant="top" src={item.avatar_url} style={{width:"15em", height:"15em"}}/>
+            <Card.Body>
+            <Card.Title>{item.login}</Card.Title>
+            <Card.Text>
+              
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            {item.type}
+          </Card.Footer>
+          </>
+        ))}
+    
+  </Card>
+  
+   
+</CardGroup>
+
+    </div>
+  )
+}
+}
+export default GithubUser
